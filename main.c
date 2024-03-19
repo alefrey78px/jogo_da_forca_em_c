f96253532dc45e9b4cc78778fc2fc875cc29801d
Alexandre Frey <ale.bsd@gmail.com>
2024-03-19 19:40:21 -0300

    Adicionando comentarios

--- main.c
+++ main.c
@@ -5,6 +5,7 @@
 #include <time.h>
 
 
+// Variáveis utilizadas no jogo.
 char palavra[20];
 char dica[50];
 char palavra_secreta[20];
@@ -12,13 +13,14 @@ char letra;
 int chances = 7;
 int tamanho_palavra;
 
-struct PalavraDica {
+// Estrutura para armazenar a palavra e a respectiva dica.
+struct palavra_dica {
     char palavra[20];
     char dica[50];
 };
 
-
-struct PalavraDica banco_palavras[] = {
+// Banco de palavras e dicas
+struct palavra_dica banco_palavras[] = {
         {"abacaxi",    "fruta tropical"},
         {"pera",       "fruta comum"},
         {"morango",    "fruta vermelha"},
@@ -33,17 +35,18 @@ struct PalavraDica banco_palavras[] = {
 
 
 void sorteia_palavra() {
-    srand(time(NULL)); // Inicializa o gerador de números aleatórios com uma semente diferente
+    srand(time(NULL)); // Inicializa o gerador de números aleatórios com uma semente diferente.
 
-    int numero = rand() % 10; // Gera um número aleatório entre 0 e 9
+    int numero = rand() % 10; // Gera um número aleatório entre 0 e 9.
 
-    struct PalavraDica pd = banco_palavras[numero];
+    struct palavra_dica pd = banco_palavras[numero];
 
     strcpy(palavra, pd.palavra);
     strcpy(dica, pd.dica);
 
     tamanho_palavra = strlen(palavra);
 
+    // Preenche a variavel palavra_secreta com um - para cada letra da palavra.
     for (int i = 0; i < tamanho_palavra; i++) {
         palavra_secreta[i] = '-';
     }
@@ -52,12 +55,16 @@ void sorteia_palavra() {
 }
 
 
+// Função para pedir o chute, basicamente le uma letra pelo teclado.
+// Não possui nenhuma verificação se o usuário digitou apenas uma letra,
+// caso o mesmo digite algo incorreto irá dar erro.
 void pede_chute() {
     printf("\nChute uma letra: ");
     scanf(" %c", &letra);
 }
 
 
+// Essa função verifica a letra chutada, ou melhor, se ela existe na palavra.
 void verifica_chute() {
     int letraEncontrada = 0;
 
@@ -75,11 +82,20 @@ void verifica_chute() {
 }
 
 
+// Quando palavra e palavra_secreta forem iguais irá retornar 1 (true).
 bool venceu() {
-    return strcmp(palavra, palavra_secreta) == 0;
+    if (strcmp(palavra, palavra_secreta) == 0 ) {
+        return 1;
+    } else {
+        return 0;
+    }
+
+    // Assim também funciona mas em minha opinião é menos didático.
+    //return strcmp(palavra, palavra_secreta) == 0;
 }
 
 
+// Vai exibindo a tela do jogo cada vez que chamado.
 void exibe() {
     printf("\nJogo Da Forca\n");
     printf("\n|==========|\n"
@@ -146,26 +162,28 @@ void exibe() {
 }
 
 
-
 // Inicio do programa principal.
 void main() {
 
-    sorteia_palavra();
+    sorteia_palavra(); // Faz o sorteio da palavra e da respectiva dica.
 
+    // Enquanto tem chances vai jogando.
     while (chances > 0) {
 
-        exibe();
+        exibe(); // Exibe a tela do jogo.
 
-        pede_chute();
+        pede_chute(); // Pede o chute.
 
-        verifica_chute();
+        verifica_chute(); // Verifica o chute.
 
+        // Se venceu exibe a tela e mensagem de vencedor.
         if (venceu()) {
             exibe();
             printf("\nParabens! Voce acertou a palavra secreta!\n");
             break;
         }
 
+        // Se acabaram as chances então game over.
         if (chances == 0) {
             exibe();
             printf("\nPerdeu!, a palavra era: %s\n", palavra);
@@ -174,4 +192,3 @@ void main() {
     }
 
 }
-
